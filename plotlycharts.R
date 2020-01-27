c5ec82b8570a1d2a9ac4cc11012dbbcbcc61c863
Hamza.Musse <hmzmusse@gmail.com>
2020-01-27 13:31:24 -0500

    categorical data viz. 1 and 2 variables.

--- plotlycharts.R
+++ plotlycharts.R
@@ -1,3 +1,16 @@
 ##Interactive Data Viz
 
 #Import plotly
+library(plotly)
+data(diamonds, package= "ggplot2")
+
+str(diamonds)
+head(diamonds)
+
+#Visualizing categorical data
+
+cut_freq <- plot_ly(diamonds, x = ~cut)
+cut_freq
+
+clarity_freq_bycut <- plot_ly(diamonds, x=~cut, color=~clarity)
+clarity_freq_bycut
